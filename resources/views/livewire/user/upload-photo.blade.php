206524911e3bd6c78f61228f48d99f66256c32fb
José Filho <josefilhocontatos0@gmail.com>
2025-01-19 15:11:57 -0300

    Adding a image spoiler upload

--- resources/views/livewire/user/upload-photo.blade.php
+++ resources/views/livewire/user/upload-photo.blade.php
@@ -1,6 +1,11 @@
 <div>
     <form action="#" method="post" wire:submit.prevent="storagePhoto">
         <h3>Atualizar imagem do perfil</h3>
+        
+        @if($photo)
+            <img src="{{ $photo->temporaryUrl() }}" alt="{{ $photo->getClientOriginalName() }}">
+        @endif
+
         <input type="file" wire:model="photo"> <br>
         @error('photo')
             <span>{{ $message }}</span>
